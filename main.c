a4ed15af2c9b7bb5b7fea717a3c8f9bad212a75a
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-23 23:06:45 -0500

    Change test program

--- main.c
+++ main.c
@@ -1,44 +1,52 @@
 #include <stdio.h>
-#include <stdbool.h>
+#include <stdlib.h>
 #include "src/terminal.h"
+struct entity {
+	char sym;
+	gr_t mode;
+};
+struct tile {
+	struct entity *e;
+	char fg_sym,bg_sym;
+	gr_t fg_mode,bg_mode;
+};
+#define WIDTH 80
+#define HEIGHT 24
+#define AREA (WIDTH*HEIGHT)
+struct tile zone[AREA];
+void draw_tile(struct tile *t)
+{
+	sgr(RESET);
+	if (t->e) {
+		mode(t->e->mode);
+		putchar(t->e->sym);
+	} else if (t->fg_sym) {
+		mode(t->fg_mode);
+		putchar(t->fg_sym);
+	} else if (t->bg_sym) {
+		mode(t->bg_mode);
+		putchar(t->bg_sym);
+	}
+}
+void draw_zone(struct tile *z)
+{
+	for (int i=0;i<AREA;i++) {
+		move_cursor(i%WIDTH,i/WIDTH);
+		draw_tile(&z[i]);
+	}
+}
+gr_t grass_modes[2]={{FG_GREEN,BG_BLACK},{FG_GREEN,BG_BLACK,BOLD}};
+char grass_syms[8]="\"';:.,`";
 int main(int argc,char **argv)
 {
-	gr_t p={FG_GREEN,BG_GREEN,ITALIC,BOLD,STRIKETHROUGH,UNDERLINE};
-	gr_t n={BOLD,FG_YELLOW,BG_BLUE};
-	clear_screen();
-	int x=80,y=24;
-	set_canon(false);
-	set_cursor_visible(false);
-	char input='\0';
-	while (input!=4) {
-		move_cursor(x,y);
-		mode(p);
-		putchar('@');
-		input=fgetc(stdin);
-		move_cursor(x,y);
-		sgr(RESET);
-		putchar(' ');
-		move_cursor(0,25);
-		clear_line();
-		mode(n);
-		printf("%d",input);
-		switch (input) {
-		case 'h':
-			x--;
-			break;
-		case 'j':
-			y++;
-			break;
-		case 'k':
-			y--;
-			break;
-		case 'l':
-			x++;
-		}
+	gr_t grass_mode={FG_GREEN,BG_BLACK,0};
+	for (int i=0;i<AREA;i++) {
+		grcpy(zone[i].bg_mode,grass_modes[rand()%2]);
+		zone[i].bg_sym=grass_syms[rand()%7];
 	}
-	set_canon(true);
-	set_cursor_visible(true);
-	move_cursor(0,26);
+	clear_screen();
+	draw_zone(zone);
 	sgr(RESET);
+	putchar('\n');
 	return 0;
 }
