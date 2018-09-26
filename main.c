de97ec39eb71135c601be38856ab6873da3e4ec7
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-09-26 13:32:58 -0500

    More complex testing

--- main.c
+++ main.c
@@ -8,7 +8,6 @@
 #define WIDTH 80
 #define HEIGHT 24
 #define AREA (WIDTH*HEIGHT)
-struct tile zone[AREA];
 void draw_zone(struct tile *z)
 {
 	for (int i=0;i<AREA;i++) {
@@ -16,10 +15,48 @@ void draw_zone(struct tile *z)
 		draw_tile(&z[i]);
 	}
 }
+void draw_pos(struct tile *z,int pos)
+{
+	move_cursor(pos%WIDTH,pos/WIDTH);
+	draw_tile(&z[pos]);
+}
 char grass_syms[]="\"';:.,`";
 int n_grass_syms=sizeof(grass_syms)/sizeof(grass_syms[0])-1;
+bool legal_move(int from,int to)
+{
+	int dx=to%WIDTH-from%WIDTH;
+	int y=to/WIDTH;
+	return -1<=dx&&dx<=1&&0<=y&&y<HEIGHT;
+}
+void move_entity(struct tile *z,int from,int to)
+{
+	struct entity *e=z[from].e;
+	z[from].e=NULL;
+	draw_pos(z,from);
+	z[to].e=e;
+	draw_pos(z,to);
+}
+int handle_move(struct tile *z,int from,char input)
+{
+	static int offsets[10]={0,
+		WIDTH-1,WIDTH,WIDTH+1,
+		-1,0,1,
+		-WIDTH-1,-WIDTH,-WIDTH+1};
+	int to=from;
+	if ('0'<=input&&input<='9')
+		to=from+offsets[input-'0'];
+	if (!legal_move(from,to))
+		to=from;
+	move_entity(z,from,to);
+	return to;
+}
+////////////////////////////////////////////////////////////////
+struct tile zone[AREA];
+int player_coords=50;
+struct entity player={.name="Player",.gr={FG_BLUE,BOLD},.sym='@'};
 int main(int argc,char **argv)
 {
+	zone[50].e=&player;
 	srand(time(NULL));
 	for (int i=0;i<argc;i++)
 		printf("%s ",argv[i]);
@@ -32,6 +69,17 @@ int main(int argc,char **argv)
 	draw_zone(zone);
 	sgr(RESET);
 	putchar('\n');
+	char input='\0';
+	set_canon(false);
+	set_cursor_visible(false);
+	while (input!=4) {
+		input=fgetc(stdin);
+		player_coords=handle_move(zone,player_coords,input);
+	}
+	set_canon(true);
+	set_cursor_visible(true);
+	move_cursor(0,HEIGHT);
+	sgr(RESET);
 #define PUT_SIZEOF(type) printf("sizeof("#type"): %lu\n",sizeof(type))
 	PUT_SIZEOF(struct entitytype);
 	PUT_SIZEOF(struct entity);
