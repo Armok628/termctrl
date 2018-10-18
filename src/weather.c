23f3fa3ccabcdb229753710d097c0007ecb678a9
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-10-18 10:10:55 -0500

    Better weather effects for scrolling

--- src/weather.c
+++ src/weather.c
@@ -1,7 +1,7 @@
 #include "weather.h"
-#define N_PARTICLES 10
+#define N_PARTICLES (Z_AREA/192)
 void (*weather)(void)=NULL;
-void precip(char *chrs,color_t c,int len)
+void precip(color_t c,char fg)
 {
 	static int pos[N_PARTICLES]={0},next=0;
 	static clock_t last_update=0;
@@ -13,19 +13,19 @@ void precip(char *chrs,color_t c,int len)
 		pos[next]=rand()%Z_AREA;
 		for (int i=0;i<N_PARTICLES;i++) {
 			int p=pos[i];
-			int x=p%Z_WIDTH+z_offset_x,y=p/G_WIDTH+z_offset_y;
-			if (x>0&&x<Z_WIDTH&&y>0&&y<Z_HEIGHT) {
+			int x=p%Z_WIDTH+z_offset_x,y=p/Z_WIDTH+z_offset_y;
+			if (x>0&&x<G_WIDTH&&y>0&&y<G_HEIGHT) {
 				move_cursor(x,y);
 				set_fg(c);
-				putchar(chrs[rand()%len]);
+				putchar(fg);
 			}
 		}
 #else
 		int p=rand()%Z_AREA;
 		pos[next]=p;
-		move_cursor(p%G_WIDTH,p/G_WIDTH);
+		move_cursor(p%Z_WIDTH,p/Z_WIDTH);
 		set_fg(c);
-		putchar(chrs[rand()%len]);
+		putchar(fg);
 #endif
 		if (++next>=N_PARTICLES)
 			next=0;
@@ -33,9 +33,9 @@ void precip(char *chrs,color_t c,int len)
 }
 void rain(void)
 {
-	precip("/|\\",BLUE,3);
+	precip(LIGHT_BLUE,',');
 }
 void snow(void)
 {
-	precip("*.",WHITE,2);
+	precip(WHITE,'*');
 }
