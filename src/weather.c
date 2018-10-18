31f712b31e78581864504e99cedeafadc308f3e4
Mark Johnson <Mark.Johnson.62899@gmail.com>
2018-10-17 23:06:52 -0500

    Fix weather for scrolling

--- src/weather.c
+++ src/weather.c
@@ -10,11 +10,20 @@ void rain(void)
 	if (now-last_update>10000) {
 		last_update=now;
 		draw_pos(current_zone,pos[next]);
-		int i=rand()%G_AREA;
+		int i=rand()%Z_AREA;
 		pos[next]=i;
+#ifdef SCROLL
+		int x=i%Z_WIDTH+z_offset_x,y=i/G_WIDTH+z_offset_y;
+		if (x>0&&x<Z_WIDTH&&y>0&&y<Z_HEIGHT) {
+			move_cursor(x,y);
+			set_fg(BLUE);
+			putchar(drops[rand()%3]);
+		}
+#else
 		move_cursor(i%G_WIDTH,i/G_WIDTH);
 		set_fg(BLUE);
 		putchar(drops[rand()%3]);
+#endif
 		if (++next>=N_PARTICLES)
 			next=0;
 	}
@@ -27,13 +36,21 @@ void snow(void)
 	if (now-last_update>10000) {
 		last_update=now;
 		draw_pos(current_zone,pos[next]);
-		int i=rand()%G_AREA;
+		int i=rand()%Z_AREA;
 		pos[next]=i;
+#ifdef SCROLL
+		int x=i%Z_WIDTH+z_offset_x,y=i/G_WIDTH+z_offset_y;
+		if (x>0&&x<Z_WIDTH&&y>0&&y<Z_HEIGHT) {
+			move_cursor(x,y);
+			set_fg(WHITE);
+			putchar(rand()%2?'*':'.');
+		}
+#else
 		move_cursor(i%G_WIDTH,i/G_WIDTH);
 		set_fg(WHITE);
 		putchar(rand()%2?'*':'.');
+#endif
 		if (++next>=N_PARTICLES)
 			next=0;
 	}
 }
-
