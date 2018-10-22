3a7e541617a8d2cd4b883c6682019041dabdc013
Armok628 <Mark.Johnson.62899@gmail.com>
2018-10-22 17:37:59 -0500

    More interesting expansion

--- src/faction.c
+++ src/faction.c
@@ -2,13 +2,12 @@
 int spread_influence(struct worldtile *w,int pos)
 {
 	int choices[9],n=0;
-	struct faction *f=w[pos].faction;
 	for (int dx=-1;dx<=1;dx++)
 		for (int dy=-1;dy<=1;dy++) {
 			int i=pos+dx+dy*W_WIDTH;
 			if (!legal_world_move(pos,i))
 				continue;
-			if (w[i].elev>=500&&w[i].faction!=f)
+			if (w[i].elev>=500)
 				choices[n++]=i;
 		}
 	if (!n)
