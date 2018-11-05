459220976837768f38277ff5a68506ac39bfcd63
Mark Johnson <Armok628>
2018-11-04 20:16:44 -0600

    Fairer expansion

--- src/faction.c
+++ src/faction.c
@@ -17,13 +17,16 @@ int spread_influence(struct worldtile *w,int pos)
 }
 void spread_faction(struct worldtile *w,struct faction *f)
 {
+	struct faction *t[W_AREA]; // Territory map
+	for (int i=0;i<W_AREA;i++)
+		t[i]=w[i].faction;
 	for (int i=0;i<W_AREA;i++) {
-		if (w[i].faction!=f)
+		if (t[i]!=f)
 			continue;
 		int target=spread_influence(w,i);
 		if (target==i)
 			continue;
-		if (!w[i].faction&&rand()%2)
+		if (!t[i]&&rand()%2)
 			w[target].faction=f;
 		else
 			switch (rand()%3) {
