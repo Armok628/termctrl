ee5187bb7b16d6434d3287e0cbf5d4c653f9ad1e
Mark Johnson <Armok628>
2019-02-05 14:27:30 -0600

    Remove demos

--- demos/age_demo.tcl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/tclsh
-set seed [expr {int(rand()*1e10)}]
-for {set i 0} {$i<=9} {incr i} {
-	exec ./a.out seed=$seed {*}$argv age=$i << [format %c 4] >&@ stdout
-	if {$i==4} {after 2000}
-	after 500
-}
--- demos/t_o_demo.tcl
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/tclsh
-set seed [expr {int(rand()*1e10)}]
-for {set i 500} {$i>=-500} {incr i -50} {
-	exec ./a.out seed=$seed {*}$argv t_o=$i << [format %c 4] >&@ stdout
-	if {$i==0} {after 2000}
-	after 500
-}
